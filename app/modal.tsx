9be8058ee653f1388293c54ced780190faeae7c4
Daniel Langio <langio.tehiniavo@gmail.com>
2026-01-28 22:31:29 +0300

    chore: messy first commit

---
+++ app/modal.tsx
@@ -0,0 +1,8 @@
+import { View } from 'react-native';
+
+export default function ModalScreen() {
+  return (
+    <View>Modal</View>
+  )
+}
+
---
+++ components/connect.tsx
@@ -0,0 +1,2 @@
+
+const BASE_API_URL = "vendredi-soir-posu.onrender.com"
